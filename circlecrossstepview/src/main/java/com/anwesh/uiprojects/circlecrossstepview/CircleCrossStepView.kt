e61b51f342c7262f89eb97632e10de95c0d72927
Anwesh43 <anweshthecool0@gmail.com>
2018-11-03 11:18:28 +0530

    adding libraries and defining nodes

--- circlecrossstepview/src/main/java/com/anwesh/uiprojects/circlecrossstepview/CircleCrossStepView.kt
+++ circlecrossstepview/src/main/java/com/anwesh/uiprojects/circlecrossstepview/CircleCrossStepView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.circlecrossstepview
 /**
  * Created by anweshmishra on 03/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5 
